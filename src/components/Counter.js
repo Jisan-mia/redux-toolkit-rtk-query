0f2ef42fb31b9ffccbb4d70eeebce3c37cf57b7d
Jisan-mia <jisanmia47@gmail.com>
2022-06-21 00:47:35 +0600

    incrementByValue and reset state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from '../redux/featues/counter/counterSlice'
+import { decrement, increment, incrementByAmount, reset } from '../redux/featues/counter/counterSlice'
 
 const Counter = () => {
+  const [countInput, setCountInput] = useState(0)
   const count = useSelector((state) => state.counter.count)
   const dispatch = useDispatch()
 
@@ -14,12 +15,31 @@ const Counter = () => {
       <div className='flex gap-4 items-center'>
         <button onClick={() => {
           dispatch(increment())
-        }} className='rounded-sm border-none outline-none focus:ring-4 focus:ring-blue-300 bg-blue-700 text-white font-bold text-3xl py-2 px-4 flex items-center justify-center' >+</button>
+        }} className='rounded-md border-none outline-none focus:ring-4 focus:ring-blue-300 bg-blue-700 text-white font-bold text-3xl py-2 px-4 flex items-center justify-center' >+</button>
         <span className='text-2xl'>{count}</span>
         <button onClick={() => {
           dispatch(decrement(4))
-        }} className='rounded-sm border-none outline-none focus:ring-4 focus:ring-blue-300 bg-blue-700 text-white font-bold text-3xl py-2 px-4 flex items-center justify-center'>-</button>
-      </div>  
+        }} className='rounded-md border-none outline-none focus:ring-4 focus:ring-blue-300 bg-blue-700 text-white font-bold text-3xl py-2 px-4 flex items-center justify-center'>-</button>
+      </div>
+      <input className='border border-gray-400 outline-none focus:ring-4 focus-within:ring-blue-300 text-xl rounded-md p-1' type="number" value={countInput} onChange={(e) => {
+        setCountInput(e.target.valueAsNumber)
+      }} />  
+     <div className='flex gap-2'>
+        <button
+          onClick={() => {
+            dispatch(incrementByAmount(countInput))
+            setCountInput(0)
+
+          }}
+          className='rounded-md border-none outline-none focus:ring-4 focus:ring-blue-300 bg-blue-700 text-white font-bold text-xl py-2 px-7 block flex items-center justify-center'
+        >Add</button>
+        <button
+          onClick={() => {
+            dispatch(reset())
+          }}
+          className='rounded-md border-none outline-none focus:ring-4 focus:ring-blue-300 bg-blue-700 text-white font-bold text-xl py-2 px-7 block flex items-center justify-center'
+        >Reset</button>
+     </div>
     </div>
   )
 }
